59e3355b636fd50110fc604159e2d418dd4115e0
Michael-Fares <mjfares@gmail.com>
2021-07-21 08:21:28 -0500

    POST method for create user finally working

--- controllers/users.js
+++ controllers/users.js
@@ -24,34 +24,41 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "INSERT INTO ?? (??, ??) VALUES (?,?); INSERT INTO ?? (??,??,??,??,??,??) VALUES (?,?,?,?,?,?); INSERT INTO ?? (??,??,??,??) VALUES (?,?,?,?);"
+  let usersSql = `
+  INSERT INTO users 
+  SET first_name = ?, last_name = ?; `
+
+  let addressSql =`INSERT INTO usersAddress 
+  SET user_id = ?, address = ?, city = ?, county = ?, state=?, zip = ?;`
+  
+  let contactSql = `INSERT INTO 
+  usersContact 
+  SET user_id = ?, phone1 = ?, phone2 = ?, email = ?; 
+  `
 
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, 
-    ['users', 
-    'first_name', 'last_name', 
-    req.body.first_name, req.body.last_name,
-    'usersAddress',
-    'user_id', 'address','city','county','state','zip', 
-    req.body.user_id, req.body.address, req.body.city, req.body.county, req.body.state, req.body.zip,
-    'usersContact',
-    'user_id','phone1','phone2','email',
-    req.body.user_id, req.body.phone1, req.body.phone2, req.body.email])
+usersSql = mysql.format(usersSql, 
+    [req.body.first_name, req.body.last_name])
 
-  pool.query(sql, (err, results) => {
-    if (err) return handleSQLError(res, err)
-    return res.json({ newId: results.insertId, 
-                      first_name: req.body.first_name, 
-                      last_name: req.body.last_name,
-                      user_id: req.body.user_id,
-                      address: req.body.address,
-                      city: req.body.city,
-                      county: req.body.county,
-                      state: req.body.state,
-                      zip: req.body.zip,
-                      phone1: req.body.phone1,
-                      phone2: req.body.phone2,
-                      email: req.body.email});
+addressSql = mysql.format( addressSql, 
+    [req.body.user_id, req.body.address, req.body.city, req.body.county, req.body.state, req.body.zip,
+    ]);
+
+contactSql = mysql.format(contactSql, 
+    [req.body.user_id, req.body.phone1, req.body.phone2, req.body.email])
+
+
+
+// your "nested pool query method worked!!! what a life saver!"
+
+  pool.query(usersSql, (err, results) => {
+    pool.query(addressSql, (err, results) => {
+      pool.query(contactSql, (err, results) => {
+        if (err) return handleSQLError(res, err)
+        return res.json({ newId: results.insertId, 
+                          ...req.body});
+      })
+    })  
   })
 }
 
